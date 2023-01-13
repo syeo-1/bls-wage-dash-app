1e6b48d3611cc103b087649b1f3e43d26d0706f0
Sean Yeo <seansqyeo@gmail.com>
2023-01-12 20:56:29 -0500

    can print out form values on clicking submit button

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from dash import Dash, dcc, html, Input, Output
+from dash import Dash, dcc, html, Input, Output, State
 import pandas as pd
 import sqlite3
 
@@ -34,9 +34,35 @@ app.layout = html.Div([
     dcc.Dropdown(counties, placeholder='type in a county!', id='county-names'),
     'Job',
     dcc.Dropdown(occupation_titles, placeholder='type in a job!', id='occupation-titles'),
-    html.Button('Add To Graph', id='add-data-to-graph')
+    html.Button(id='submit-button-state', n_clicks=0, children='Submit'),
+    html.Div(id='output-state')
 ])
 
+# this callback is ripped from here: https://dash.plotly.com/basic-callbacks. Check the submit button section to finish this off
+# for the case of giving montreal, canada as the input
+@app.callback(Output('output-state', 'children'),
+              Input('submit-button-state', 'n_clicks'),
+              State('county-names', 'value'),
+              State('occupation-titles', 'value'))
+def update_output(n_clicks, input1, input2):
+    return u'''
+        The Button has been pressed {} times,
+        Input 1 is "{}",
+        and Input 2 is "{}"
+    '''.format(n_clicks, input1, input2)
+
+# next steps:
+# need to use the county that was entered and the occupation to retrieve the appropriate data from the wage dataframe
+
+# from there, need to somehow take that data and graph it out
+    # at this point probably stick to graphing each column of the row in separate graphs
+    # ie. wage, employment data, etc... will each have their own graph
+
+# for now, using the user entered data, try just to print out the corresponding row in the wage dataframe. That'll be pretty cool already!
+
+# actually, for now,just try to print out the values the user gave first on clicking the button!
+
+# check out the section in basic callbacks where 
 
 
 if __name__ == '__main__':
