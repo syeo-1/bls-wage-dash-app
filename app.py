db59a958a680a180c8d1eb17a564f9ce2f77c377
syeo-1 <52718265+syeo-1@users.noreply.github.com>
2023-01-22 17:52:36 -0500

    Update app.py

--- app.py
+++ app.py
@@ -12,6 +12,7 @@ external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
 
 
 app = Dash(__name__, external_stylesheets=external_stylesheets)
+server=app.server
 # app = Dash(__name__, external_stylesheets=[dbc.themes.BOOTSTRAP, dbc_css])
 
 # create connection to the database first
