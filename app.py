094b6b616998f0494329259289c6dfc8ca6d074a
Sean Yeo <seansqyeo@gmail.com>
2023-01-29 18:32:59 -0500

    set debug to false

--- app.py
+++ app.py
@@ -244,4 +244,5 @@ def update_output(n_clicks, _, children, county_name, occupation_title, county_j
 
 
 if __name__ == '__main__':
-    app.run_server(debug=True)
+    # app.run_server(debug=True)
+    app.run_server(debug=False)
